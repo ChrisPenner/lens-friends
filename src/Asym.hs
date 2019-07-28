0823776a5735eb98d59f14d85305215387923bc6
Chris Penner <christopher.penner@gmail.com>
2019-07-27 18:48:22 -0600

    Experiment with asymmetric lenses

--- src/Asym.hs
+++ src/Asym.hs
@@ -1,16 +1,47 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DerivingStrategies #-}
 module Asym where
 
 import Control.Lens
 import Control.Applicative
+import Data.Monoid
 
-sliceT ::
-  (Traversable t, Applicative f) =>
-  Int -> Int -> Int -> (a -> f a) -> t a -> f (t a)
-sliceT start end stepSize f xs = xs & traversed . indices checkIndex %%~ f
-  where
-    size = length xs
-    start' = if start >= 0 then start else size + start
-    end' = if end >= 0 then end else size + end
-    checkIndex i = i >= start' && i < end' && ((i - start') `mod` stepSize == 0)
+data User = User {_name :: String, _address :: String}
+    deriving Show
+newtype Form = Form [String]
+    deriving (Show)
+    deriving newtype (Semigroup, Monoid)
+makeLenses ''User
+
+injForm :: String -> Form
+injForm = Form . pure
+
+oneWay :: (a -> b) -> Lens a r b r
+oneWay g f a = f (g a)
+
+restrict :: Getter s a -> Lens s r a r
+restrict l = oneWay (view l)
+
+userName :: Lens User r String r
+userName = oneWay _name
+
+userAddress :: Lens User r String r
+userAddress = oneWay _address
+
+cap :: Traversal s t a b -> (a -> b) -> Traversal s t x y
+cap l g _f s = l (pure . g) s
+
+(<->) :: (Semigroup t, Applicative f) => LensLike f s t a b -> LensLike f s t a b -> LensLike f s t a b
+(<->) alens blens f s = liftA2 (<>) (alens f s) (blens f s)
+
+form :: Traversal User Form Form Form
+form = cap userName injForm <-> cap userAddress injForm
+
+joe :: User
+joe = User "Joe" "Wallaby Way"
+
+run :: LensLike Identity s a a a -> s -> a
+run l = runIdentity . l pure
--- src/Slicing.hs
+++ src/Slicing.hs
@@ -3,14 +3,14 @@
 module Slicing where
 
 import Control.Lens
-import Control.Applicative
 
-sliceT :: (Traversable t, Applicative f, Reversing (t a))
+sliced :: Traversable t => Int -> Int -> Int -> Traversal' (t a) a
+sliced start end step = partsOf traversed . sliced' start end step
+
+sliced' :: (Traversable t, Reversing (t a))
        => Int -> Int -> Int
-       -> (a -> f a)
-       -> t a
-       -> f (t a)
-sliceT start end stepSize f xs =
+       -> Traversal' (t a) a
+sliced' start end stepSize f xs =
     xs
     & partsOf (traversed . indices pickElements)
     . maybeReversed
@@ -24,3 +24,16 @@ sliceT start end stepSize f xs =
     maybeReversed = if stepSize < 0 then reversed else id
     pickElements i = i >= (if stepSize >= 0 then start' else end') && i < (if stepSize >= 0 then end' else start')
     stepper i = i `mod` abs stepSize == 0
+
+-- >>> [1..10] ^.. sliced 5 2 (-1)
+-- [5,4,3]
+-- >>> [1..10] ^.. sliced 0 5 1
+-- [1,2,3,4,5]
+-- >>> [1..10] ^.. sliced 0 10 2
+-- [1,3,5,7,9]
+-- >>> [1..10] ^.. sliced 4 10 2
+-- [5,7,9]
+-- >>> [1..10] ^.. sliced 10 0 (-2)
+-- [10,8,6,4,2]
+-- >>> [1..10] ^.. sliced 0 (-3) 1
+-- [1,2,3,4,5,6,7]
