5cdf7b0105f9923b7b4c7ad417dec1b8c70de88d
Chris Penner <christopher.penner@gmail.com>
2019-07-27 20:51:59 -0600

    Updates

--- src/Asym.hs
+++ src/Asym.hs
@@ -8,6 +8,7 @@ module Asym where
 import Control.Lens
 import Control.Applicative
 import Data.Monoid
+import Data.Void
 
 data User = User {_name :: String, _address :: String}
     deriving Show
@@ -16,32 +17,55 @@ newtype Form = Form [String]
     deriving newtype (Semigroup, Monoid)
 makeLenses ''User
 
+type Refraction f r s a = LensLike f s r a r
+type Refraction' f r s = Refraction f r s Void
+
 injForm :: String -> Form
 injForm = Form . pure
 
 oneWay :: (a -> b) -> Lens a r b r
 oneWay g f a = f (g a)
 
-restrict :: Getter s a -> Lens s r a r
+restrict :: Functor f => Getter s a -> Refraction f r s a
 restrict l = oneWay (view l)
 
-userName :: Lens User r String r
-userName = oneWay _name
+userName :: Functor f => Refraction f r User String
+userName = restrict name
 
-userAddress :: Lens User r String r
-userAddress = oneWay _address
+userAddress :: Functor f => Refraction f r User String
+userAddress = restrict address
+
+inject :: Functor f => (s -> r -> r) -> Refraction f r s s
+inject g f s = g s <$> f s
 
 cap :: Traversal s t a b -> (a -> b) -> Traversal s t x y
 cap l g _f s = l (pure . g) s
 
+capped :: Applicative f => (s -> r) -> Refraction f r s x
+capped g f s = pure $ g s
+
+capped' :: (Monoid r, Applicative f) => Refraction f r s Void
+capped' f s = pure mempty
+
+capped'' :: Applicative f => Refraction f r r ()
+capped'' = capped id
+
+infixr 8 <->
 (<->) :: (Semigroup t, Applicative f) => LensLike f s t a b -> LensLike f s t a b -> LensLike f s t a b
 (<->) alens blens f s = liftA2 (<>) (alens f s) (blens f s)
 
-form :: Traversal User Form Form Form
-form = cap userName injForm <-> cap userAddress injForm
+form :: Applicative f => Refraction f Form User Void
+form = userName . capped injForm <-> userAddress . inject appendForm . capped'
+
+appendForm :: String -> Form -> Form
+appendForm s f = Form [s] <> f
 
 joe :: User
 joe = User "Joe" "Wallaby Way"
 
-run :: LensLike Identity s a a a -> s -> a
+run :: Refraction Identity a s a -> s -> a
 run l = runIdentity . l pure
+
+run' :: Refraction Identity a s Void -> s -> a
+run' l = runIdentity . l absurd
+
