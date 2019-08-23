6cbd7df16a93d49688af60568ef65cdf1743410b
Chris Penner <christopher.penner@gmail.com>
2019-08-23 11:18:22 -0600

    Add some more putter sketches

--- src/Putter.hs
+++ src/Putter.hs
@@ -12,15 +12,16 @@ module Putter where
 
 import Control.Lens
 
-type Putter s a = Setter s s () a
+type Putter' s a = Setter s s () a
+-- type Putter s t a = Setter s t () a
 
-append :: forall s a. Snoc s s a a => Putter s a
+append :: forall s a. Snoc s s a a => Putter' s a
 append = _Snoc . sets putter
   where
     putter :: (() -> a) -> (s, a) -> (s, a)
     putter f (s, a) = (s |> a, f ())
 
-prepend :: forall s a. Cons s s a a => Putter s a
+prepend :: forall s a. Cons s s a a => Putter' s a
 prepend = _Cons . sets putter
   where
     putter :: (() -> a) -> (a, s) -> (a, s)
@@ -29,8 +30,40 @@ prepend = _Cons . sets putter
 splittingAt :: Int -> Iso' [a] ([a], [a])
 splittingAt i = iso (splitAt i) (uncurry (<>))
 
-insertAt :: Int -> Putter [a] a
+insertAt :: Int -> Putter' [a] a
 insertAt i = splittingAt i . sets putter
   where
     putter f (before, after) = (before, f () : after)
 
+-- >>> Left () & pured .~ "hi"
+-- Right "hi"
+-- >>> Just 3 & pured .~ "hi"
+-- Just "hi"
+-- >>> Nothing & pured .~ "hi"
+-- Just "hi"
+
+-- >>> (1, Left ()) & _2  ??~ "hi" :: (Int, Either () String)
+-- (1,Right "hi")
+-- >>> (1, Left ()) & _2  ??~ "hi" :: (Int, Maybe String)
+-- (1,Just "hi")
+-- >>> (1, Left ()) & _2  ??~ "hi" :: (Int, [String])
+-- (1,["hi"])
+
+----------------------------------------------------------
+
+type Putter t a = forall s. Setter s t () a
+
+pured :: Applicative f => Putter (f b) b
+pured = sets putter
+  where
+    putter f _ = pure (f ())
+
+just :: Putter (Maybe b) b
+just = pured
+
+right :: Putter (Either e b) b
+right = pured
+
+-- Generalize `?~` to Applicative
+(??~) :: Applicative f => ASetter s t a (f b) -> b -> s -> t
+(??~) l b = l . pured .~ b
