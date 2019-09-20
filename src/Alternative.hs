30562304a88aec258e884f760903f6350fb8e578
Chris Penner <christopher.penner@gmail.com>
2019-09-20 00:13:43 -0600

    Updates

--- src/Alternative.hs
+++ src/Alternative.hs
@@ -8,15 +8,20 @@ import Data.Foldable
 import Debug.Trace
 import Data.Either.Validation
 import Data.Semigroup
+import Data.Functor
 
-alternately :: (Alternative f) => (s -> [a]) -> LensLike f s t a t
+alternately :: (Alternative f, Foldable f) => (s -> f a) -> LensLike f s t a t
 alternately fork' f s = asum $ fmap f (fork' s)
 
-alternately' :: (Alternative f, Monad f) => (s -> [f a]) -> LensLike f s t a t
+-- No good?
+alternately' :: (Foldable g, Alternative f, Monad f) => (s -> g (f a)) -> LensLike f s t a t
 alternately' fork' f s = asum (fork' s) >>= f
 
+alternated :: (Foldable f, Alternative f) => LensLike f (f a) (f b) a b
+alternated f s = pure . asum $ (f <$> toList s)
+
 nonDetOver :: Alternative f => (a -> b) -> LensLike f s t a b -> s -> f t
-nonDetOver f l = l (pure . f)
+nonDetOver f l = l %%~ (pure . f)
 
 test :: [(String, Integer)]
 test = nonDetOver (const 'x') (_1 . alternately (\x -> [x, map toUpper x]) . taking 3 traverse) $ ("testing", 3)
@@ -44,6 +49,14 @@ infixr 4 ^|..
     Success _ -> []
     Failure a -> a
 
+infixr 4 .|!
+(.|!) :: Alternative f => LensLike f s t a a -> (a -> Bool) -> s -> f t
+(.|!) l p = l (\a -> if p a then pure a else empty)
+
+infixr 4 %%|
+(%%|) :: Alternative f => LensLike f s t a b -> (a -> f b) -> s -> f t
+(%%|) = (%%~)
+
 -- >>> (5 :: Int) ^|.. alternately (\n -> [n, n+1, n+2, n+3]) . guarding even
 -- [6,8]
 
@@ -52,8 +65,20 @@ guarding :: Alternative f => (a -> Bool) -> LensLike' f a a
 guarding p f s | p s = f s
                | otherwise = empty
 
-otherTest :: Maybe (String, Int)
+otherTest :: [(String, Int)]
 otherTest = ("testing", 3) & (_1 . alternately (\x -> [x, map toUpper x]) . taking 3 traverse) . guarding (isUpper) .|~ 'c'
 
 guardTest :: [(String, Int)]
 guardTest = ("testing", 3) & (_1 . alternately (\x -> [x, map toUpper x]) . taking 3 traverse) . guarding (isLower) %|~ const 'c'
+
+
+-- >>> ('a', [Just "hi", Nothing]) & _2 . alternated .|! const False
+-- [('a',[])]
+-- >>> ('a', [Just "hi", Nothing]) & _2 . alternated .|! const True
+-- [('a',[Just "hi",Nothing])]
+-- >>> ('a', [Just "hi", Nothing]) & _2 . traversed . alternated .|!  const False
+-- Just ('a',[Nothing,Nothing])
+-- >>> ('a', [Just "hi", Nothing]) & _2 .  alternately' id .|!  const False
+-- Nothing
+-- >>> ('a', [Just "hi", Nothing]) & _2 .  alternately' id .|!  const True
+-- Just ('a',"hi")
