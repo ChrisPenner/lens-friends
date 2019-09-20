edc5acb43222f52e83fc09cfe06a0fb8749c41da
Chris Penner <christopher.penner@gmail.com>
2019-09-19 23:22:01 -0600

    Add some viewing actions for Altertative lenses

--- src/Alternative.hs
+++ src/Alternative.hs
@@ -1,4 +1,4 @@
-module NonDet where
+module Alternative where
 
 import Control.Lens
 import Control.Monad
@@ -6,34 +6,54 @@ import Data.Char
 import Control.Applicative
 import Data.Foldable
 import Debug.Trace
+import Data.Either.Validation
+import Data.Semigroup
 
-forked :: (Foldable f, Alternative f) => (s -> [s]) -> LensLike' f s s
-forked fork' f s = asum $ fmap f (fork' s)
+alternately :: (Alternative f) => (s -> [a]) -> LensLike f s t a t
+alternately fork' f s = asum $ fmap f (fork' s)
 
--- forked :: Alternative f => (s -> f s) -> LensLike' f s s
--- forked fork' f s = f (fork' s)
+alternately' :: (Alternative f, Monad f) => (s -> [f a]) -> LensLike f s t a t
+alternately' fork' f s = asum (fork' s) >>= f
 
-
-nonDetOver :: Alternative f => (a -> a) -> LensLike' f s a -> s -> f s
+nonDetOver :: Alternative f => (a -> b) -> LensLike f s t a b -> s -> f t
 nonDetOver f l = l (pure . f)
 
 test :: [(String, Integer)]
-test = nonDetOver (const 'x') (_1 . forked (\x -> [x, map toUpper x]) . taking 3 traverse) $ ("testing", 3)
+test = nonDetOver (const 'x') (_1 . alternately (\x -> [x, map toUpper x]) . taking 3 traverse) $ ("testing", 3)
 
 infixr 4 %|~
-(%|~) :: Alternative f => LensLike' f s a -> (a -> a) -> s -> f s
+(%|~) :: Alternative f => LensLike f s t a b -> (a -> b) -> s -> f t
 (%|~) = flip nonDetOver
 
 infixr 4 .|~
-(.|~) :: Alternative f => LensLike' f s a -> a -> s -> f s
+(.|~) :: Alternative f => LensLike f s t a b -> b -> s -> f t
 (.|~) l b = nonDetOver (const b) l
 
+infixr 4 ^|?
+(^|?) :: s -> LensLike' (Validation (Maybe (First a))) s a -> Maybe a
+(^|?) s l = case l (Failure . Just . First) s of
+    Success _ -> Nothing
+    Failure a -> getFirst <$> a
+
+-- >>> (5 :: Int) ^|? alternately (\n -> [n, n+1, n+2]) . guarding even
+-- Just 6
+
+infixr 4 ^|..
+(^|..) :: s -> LensLike' (Validation [a]) s a -> [a]
+(^|..) s l = case l (Failure . pure) s of
+    Success _ -> []
+    Failure a -> a
+
+-- >>> (5 :: Int) ^|.. alternately (\n -> [n, n+1, n+2, n+3]) . guarding even
+-- [6,8]
+
+-- | Unlike 'filtered', 'guarding' can fail an ENTIRE traversal fork, not just a single slice of it; e.g. it bubbles up to the last 'fork'.
 guarding :: Alternative f => (a -> Bool) -> LensLike' f a a
 guarding p f s | p s = f s
                | otherwise = empty
 
 otherTest :: Maybe (String, Int)
-otherTest = ("testing", 3) & (_1 . forked (\x -> [x, map toUpper x]) . taking 3 traverse) . guarding (isUpper) .|~ 'c'
+otherTest = ("testing", 3) & (_1 . alternately (\x -> [x, map toUpper x]) . taking 3 traverse) . guarding (isUpper) .|~ 'c'
 
 guardTest :: [(String, Int)]
-guardTest = ("testing", 3) & (_1 . forked (\x -> [x, map toUpper x]) . taking 3 traverse) . guarding (isLower) %|~ const 'c'
+guardTest = ("testing", 3) & (_1 . alternately (\x -> [x, map toUpper x]) . taking 3 traverse) . guarding (isLower) %|~ const 'c'
